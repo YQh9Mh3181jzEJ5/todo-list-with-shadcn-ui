c8712201e5355d00f9ffd40fd5f3660db4d54fea
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-22 10:02:39 +0900

    fix: prettier and typescript

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import typescript from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
   { ignores: ['dist'] },
