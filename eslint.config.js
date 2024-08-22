87c5c8321027e2c965671611bb95afc12fab39c6
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-22 10:25:55 +0900

    fix eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,46 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import typescript from '@typescript-eslint/eslint-plugin';
 import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
 export default [
-  { ignores: ['dist'] },
+  js.configs.recommended,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    settings: { react: { version: '18.3' } },
     plugins: {
+      '@typescript-eslint': typescript,
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': typescript,
       prettier,
     },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
     rules: {
-      ...js.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      ...typescript.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/prop-types': 'off', // TypeScriptを使用しているため、prop-typesは不要
+      'react-refresh/only-export-components': 'off', // 必要に応じてオフにする
       'prettier/prettier': 'error',
     },
   },
