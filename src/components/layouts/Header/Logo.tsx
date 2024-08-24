30d247d6af531a229b77aebc5439fa1116c3f928
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-25 03:32:51 +0900

    delete text

--- src/components/layouts/Header/Logo.tsx
+++ src/components/layouts/Header/Logo.tsx
@@ -3,6 +3,6 @@ import { FcTodoList } from "react-icons/fc";
 export const Logo = () => (
   <a href="/" className="flex items-center gap-2">
     <FcTodoList className="w-6 h-6" />
-    <span className="text-lg font-bold ">Todo App!</span>
+    <span className="text-lg font-bold ">Todo App</span>
   </a>
 );
